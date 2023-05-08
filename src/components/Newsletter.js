598451c5c168193322c99731b21ebd29550a9f06
anilu556 <anilu556@gmail.com>
2023-05-08 08:33:29 -0600

    optimizing code

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -38,9 +38,7 @@ export const Newsletter = () => {
         const isJson = response.headers.get('content-type')?.includes('application/json');
         const data = isJson && await response.json();
 
-        // check for error response
         if (!response.ok) {
-          // get error message from body or default to response status
           const error = (data && data.message) || response.status;
           return Promise.reject(error);
         }
